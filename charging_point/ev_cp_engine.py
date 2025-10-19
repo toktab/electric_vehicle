7dd647242b73cf53070645f0fc382f5bfa5ed97a
SilHackk <160708163+SilHackk@users.noreply.github.com>
2025-10-19 15:17:37 +0200

    something new

--- charging_point/ev_cp_engine.py
+++ charging_point/ev_cp_engine.py
@@ -78,7 +78,7 @@ class EVCPEngine:
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            server_socket.bind((self.monitor_host, self.monitor_port))
+            server_socket.bind(('0.0.0.0', self.monitor_port))
             server_socket.listen(1)
 
             print(f"[{self.cp_id}] Listening for monitor on port {self.monitor_port}")
